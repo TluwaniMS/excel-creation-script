c3d652e0a8db40aabf956edd9688ffd2dae4efe2
tluwaniMS <tluwani2020@gmail.com>
2021-11-16 19:40:28 +0200

    adding-logic-to-get-total-doctors-in-specialisation

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 const { getAllFormattedSampleDoctors } = require("./services/doctors-service");
+const {
+  getTotalNumberOfDoctorsGroupedBySpeccialisation
+} = require("./services/specialisations-service");
---
+++ services/specialisations-service.js
@@ -0,0 +1,20 @@
+const { Doctors } = require("../sample-data/doctors");
+const { Specialisations } = require("../sample-data/specialisations");
+
+function getTotalNumberOfDoctorsGroupedBySpeccialisation() {
+  const formattedSpecialisations = [];
+  Specialisations.forEach((specialisation) => {
+    const doctorsLinkedToSpecialisation = Doctors.filter(
+      (doctor) => doctor.specialty === specialisation.specialisationKey
+    );
+    const totalDoctorsLinkedToSpecialisation =
+      doctorsLinkedToSpecialisation.length;
+
+    specialisation.total = totalDoctorsLinkedToSpecialisation;
+    formattedSpecialisations.push(specialisation);
+  });
+
+  return formattedSpecialisations;
+}
+
+module.exports = { getTotalNumberOfDoctorsGroupedBySpeccialisation };
