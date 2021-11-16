2792465c50cf21ae0875743c8b8ca9a76944e733
tluwaniMS <tluwani2020@gmail.com>
2021-11-16 21:04:10 +0200

    first-phase-of-adding-excel-scripting-logic

---
+++ excel-scripting/main-excel-scripting.js
@@ -0,0 +1,6 @@
+const excel4node = require("excel4node");
+
+function mainLogicForConvertingDataToExcel() {
+  const workBook = new excel4node.Workbook();
+  const workSheet = workBook.addWorksheet("Sheet 1");
+}
