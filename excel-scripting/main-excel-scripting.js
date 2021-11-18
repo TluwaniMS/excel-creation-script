5dc3aec07a1fac88a09108641a09801cf0b985d2
tluwaniMS <tluwani2020@gmail.com>
2021-11-18 07:38:24 +0200

    adding-skeleton-functions-for-creating-excel-data-tables

--- excel-scripting/main-excel-scripting.js
+++ excel-scripting/main-excel-scripting.js
@@ -4,3 +4,19 @@ function mainLogicForConvertingDataToExcel() {
   const workBook = new excel4node.Workbook();
   const workSheet = workBook.addWorksheet("Sheet 1");
 }
+
+function createDoctorsDataTable(workSheet, data) {}
+
+function createHospitalsDataTableWithTotalDoctorsCount(workSheet, data) {}
+
+function createMunicipalitiesDataTableWithHospitalsCount(workSheet, data) {}
+
+function createMunicipalitiesDataTableWithHospitalsAndLinkedDoctorsCount(
+  workSheet,
+  data
+) {}
+
+function createSpecialisationsDataTableWithTotalDoctorsCountGroupedBySpecialisation(
+  workSheet,
+  data
+) {}
