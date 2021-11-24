19df06a42ecc62c80e905e46bc37dba2d2121ebc
tluwaniMS <tluwani2020@gmail.com>
2021-11-24 08:40:12 +0200

    addding-logic-to-extract-preceeding-data-inputs

--- excel-scripting/main-excel-scripting.js
+++ excel-scripting/main-excel-scripting.js
@@ -16,61 +16,54 @@ function mainLogicForConvertingDataToExcel() {
   createDoctorsDataTable(workSheet, dataToBeScripted);
   createHospitalsDataTableWithTotalDoctorsCount(workSheet, dataToBeScripted);
   createMunicipalitiesDataTableWithHospitalsCount(workSheet, dataToBeScripted);
-  createMunicipalitiesDataTableWithHospitalsAndLinkedDoctorsCount(
-    workSheet,
-    dataToBeScripted
-  );
-  createSpecialisationsDataTableWithTotalDoctorsCountGroupedBySpecialisation(
-    workSheet,
-    dataToBeScripted
-  );
+  createMunicipalitiesDataTableWithHospitalsAndLinkedDoctorsCount(workSheet, dataToBeScripted);
+  createSpecialisationsDataTableWithTotalDoctorsCountGroupedBySpecialisation(workSheet, dataToBeScripted);
 }
 
 function createDoctorsDataTable(workSheet, data) {
-  const doctorsData = data.filter(
-    (content) => content.dataSet.title === Enumerators.Doctors
-  );
+  const doctorsData = data.filter((content) => content.dataSet.title === Enumerators.Doctors);
 
   console.log(doctorsData);
 }
 
 function createHospitalsDataTableWithTotalDoctorsCount(workSheet, data) {
-  const hospitalsData = data.filter(
-    (content) => content.dataSet.title === Enumerators.DoctorsCountInHospitals
+  const preceedingData = data.filter((content) =>
+    DataInputPreceedingTitles.SecondDataInputPreceedingTitles.includes(content.dataSet.title)
   );
+  const hospitalsData = data.filter((content) => content.dataSet.title === Enumerators.DoctorsCountInHospitals);
 
   console.log(hospitalsData);
 }
 
 function createMunicipalitiesDataTableWithHospitalsCount(workSheet, data) {
+  const preceedingData = data.filter((content) =>
+    DataInputPreceedingTitles.ThirdDataInputPreceedingTitles.includes(content.dataSet.title)
+  );
   const municipalitiesData = data.filter(
-    (content) =>
-      content.dataSet.title === Enumerators.HospitalsCountInMunicipalities
+    (content) => content.dataSet.title === Enumerators.HospitalsCountInMunicipalities
   );
 
   console.log(municipalitiesData);
 }
 
-function createMunicipalitiesDataTableWithHospitalsAndLinkedDoctorsCount(
-  workSheet,
-  data
-) {
+function createMunicipalitiesDataTableWithHospitalsAndLinkedDoctorsCount(workSheet, data) {
+  const preceedingData = data.filter((content) =>
+    DataInputPreceedingTitles.FourthDataInputPreceedingTitles.includes(content.dataSet.title)
+  );
   const municipalitiesData = data.filter(
-    (content) =>
-      content.dataSet.title ===
-      Enumerators.DoctorsCountInMunicipalitiesGroupedByHospitals
+    (content) => content.dataSet.title === Enumerators.DoctorsCountInMunicipalitiesGroupedByHospitals
   );
 
   console.log(municipalitiesData);
 }
 
-function createSpecialisationsDataTableWithTotalDoctorsCountGroupedBySpecialisation(
-  workSheet,
-  data
-) {
+function createSpecialisationsDataTableWithTotalDoctorsCountGroupedBySpecialisation(workSheet, data) {
+  const preceedingData = data.filter((content) =>
+    DataInputPreceedingTitles.FifthDataInputPreceedingTitles.includes(content.dataSet.title)
+  );
+
   const specialitiesData = data.filter(
-    (content) =>
-      content.dataSet.title === Enumerators.DoctorsCountGroupedBySpecialisation
+    (content) => content.dataSet.title === Enumerators.DoctorsCountGroupedBySpecialisation
   );
 
   console.log(specialitiesData);
