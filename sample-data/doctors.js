863d5523933a1c13e1750ed9fcbf5dc651339157
tluwaniMS <tluwani2020@gmail.com>
2021-11-25 16:10:07 +0200

    updating-doctors-sample-data

--- sample-data/doctors.js
+++ sample-data/doctors.js
@@ -3,7 +3,6 @@ const Doctors = [
     firstName: "Dean",
     lastName: "Hill",
     gender: "Male",
-    password: "dean1234",
     email: "dean@mock.com",
     hospital: "SKKN",
     specialty: "PDTN"
@@ -12,7 +11,6 @@ const Doctors = [
     firstName: "Lufuno",
     lastName: "Sekgothe",
     gender: "Female",
-    password: "lufu1234",
     email: "lufuno@mock.com",
     hospital: "PLKH",
     specialty: "CDGT"
@@ -21,7 +19,6 @@ const Doctors = [
     firstName: "Refilwe",
     lastName: "Phago",
     gender: "Female",
-    password: "refi1234",
     email: "refilwe@mock.com",
     hospital: "DNDH",
     specialty: "URLG"
@@ -30,7 +27,6 @@ const Doctors = [
     firstName: "Seba",
     lastName: "Sibika",
     gender: "Female",
-    password: "seba1234",
     email: "seba@mock.com",
     hospital: "DNDH",
     specialty: "CDGT"
@@ -39,7 +35,6 @@ const Doctors = [
     firstName: "Katlego",
     lastName: "Mamani",
     gender: "Female",
-    password: "katl1234",
     email: "katlego@mock.com",
     hospital: "SKKN",
     specialty: "PDTN"
@@ -48,7 +43,6 @@ const Doctors = [
     firstName: "Idah",
     lastName: "Leseme",
     gender: "Female",
-    password: "idah1234",
     email: "idah@mock.com",
     hospital: "MSSH",
     specialty: "PDTN"
@@ -57,7 +51,6 @@ const Doctors = [
     firstName: "Jessica",
     lastName: "Shadung",
     gender: "Female",
-    password: "jess1234",
     email: "jessica@mock.com",
     hospital: "DNDH",
     specialty: "DMTG"
@@ -66,7 +59,6 @@ const Doctors = [
     firstName: "Tshego",
     lastName: "Seoka",
     gender: "Male",
-    password: "tshe1234",
     email: "tshego@mock.com",
     hospital: "SKKN",
     specialty: "CDGT"
@@ -75,7 +67,6 @@ const Doctors = [
     firstName: "Jethro",
     lastName: "Mokgohloa",
     gender: "Male",
-    password: "jeth1234",
     email: "jethro@mock.com",
     hospital: "PLKH",
     specialty: "URLG"
@@ -84,7 +75,6 @@ const Doctors = [
     firstName: "Kgaogelo",
     lastName: "Munyangane",
     gender: "Female",
-    password: "kgao1234",
     email: "kgaogelo@mock.com",
     hospital: "MSSH",
     specialty: "DMTG"
@@ -93,7 +83,6 @@ const Doctors = [
     firstName: "Mmadi",
     lastName: "Ndlovu",
     gender: "Male",
-    password: "mmad1234",
     email: "mmadi@mock.com",
     hospital: "BTLK",
     specialty: "DMTG"
@@ -102,7 +91,6 @@ const Doctors = [
     firstName: "Abigail",
     lastName: "Sekhosana",
     gender: "Female",
-    password: "abig1234",
     email: "abigail@mock.com",
     hospital: "NTNP",
     specialty: "URLG"
@@ -111,7 +99,6 @@ const Doctors = [
     firstName: "Mmakosha",
     lastName: "Deagrela",
     gender: "Female",
-    password: "mmak1234",
     email: "mmakosha@mock.com",
     hospital: "SHPO",
     specialty: "URLG"
@@ -120,7 +107,6 @@ const Doctors = [
     firstName: "Dorcus",
     lastName: "Maruma",
     gender: "Female",
-    password: "dorc1234",
     email: "dorcus@mock.com",
     hospital: "MRBJ",
     specialty: "DMTG"
@@ -129,7 +115,6 @@ const Doctors = [
     firstName: "Denzel",
     lastName: "Mophosho",
     gender: "Male",
-    password: "denz1234",
     email: "denzel@mock.com",
     hospital: "NTNP",
     specialty: "NRLG"
@@ -138,7 +123,6 @@ const Doctors = [
     firstName: "Lerato",
     lastName: "Sehume",
     gender: "Female",
-    password: "lera1234",
     email: "lerato@mock.com",
     hospital: "SHPO",
     specialty: "RDLG"
@@ -147,7 +131,6 @@ const Doctors = [
     firstName: "Londokuhle",
     lastName: "Ndala",
     gender: "Male",
-    password: "lond1234",
     email: "londokuhle@mock.com",
     hospital: "BLYH",
     specialty: "CDGT"
@@ -156,7 +139,6 @@ const Doctors = [
     firstName: "Thovhedzo",
     lastName: "Maosa",
     gender: "Male",
-    password: "thov1234",
     email: "thovhedzo@mock.com",
     hospital: "SHPO",
     specialty: "RDLG"
@@ -165,7 +147,6 @@ const Doctors = [
     firstName: "Reneilwe",
     lastName: "Dikana",
     gender: "Female",
-    password: "rene1234",
     email: "reneilwe@mock.com",
     hospital: "MRSB",
     specialty: "NRLG"
@@ -174,7 +155,6 @@ const Doctors = [
     firstName: "Philicia",
     lastName: "Sothobe",
     gender: "Female",
-    password: "phil1234",
     email: "philicia@mock.com",
     hospital: "BLMH",
     specialty: "PSCT"
@@ -183,7 +163,6 @@ const Doctors = [
     firstName: "Eddy",
     lastName: "Manaka",
     gender: "Male",
-    password: "eddy1234",
     email: "eddy@mock.com",
     hospital: "SHPO",
     specialty: "RDLG"
@@ -192,7 +171,6 @@ const Doctors = [
     firstName: "Mathibela",
     lastName: "Serumula",
     gender: "Male",
-    password: "math1234",
     email: "mathibela@mock.com",
     hospital: "BLYH",
     specialty: "NRLG"
@@ -201,7 +179,6 @@ const Doctors = [
     firstName: "Mpho",
     lastName: "Leshabane",
     gender: "Male",
-    password: "mpho1234",
     email: "mpho@mock.com",
     hospital: "BLMH",
     specialty: "PSCT"
@@ -210,7 +187,6 @@ const Doctors = [
     firstName: "Lehlohonolo",
     lastName: "Sebata",
     gender: "Male",
-    password: "lehl1234",
     email: "lehlohonolo@mock.com",
     hospital: "VTPH",
     specialty: "RDLG"
@@ -219,7 +195,6 @@ const Doctors = [
     firstName: "Mpumi",
     lastName: "Khoza",
     gender: "Male",
-    password: "mpum1234",
     email: "mpumi@mock.com",
     hospital: "VTPH",
     specialty: "PSCT"
